e11de79926891405e69cd0b87c83653df53996ba
Wojciech Żywno <w.zywno@gmail.com>
2014-08-09 18:02:57 +0200

    fix

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var demo = angular.module("demo", ['ngResource'])
 
 demo.controller("TescoCtrl", function ($scope, $resource) {
     $scope.tesco = $resource('http://localhost:3000/:action',{
-        action: 'products.json', callback: 'JSON_CALLBACK'},
+        action: 'products.json'}, {callback: 'JSON_CALLBACK'},
         { get: {method: 'JSONP'}}
     );
     $scope.dupa = $scope.tesco.get(function(result){
